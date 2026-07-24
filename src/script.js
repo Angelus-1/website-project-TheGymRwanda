03d874482ee83ae1c0ef5755cd3f2cf0701dbd9d
NIYOMUGISHA Obed <obed6@duck.com>
2026-07-24 13:35:39 +0200

    Load navbar and footer globally

--- src/script.js
+++ src/script.js
@@ -8,3 +8,23 @@ const loadComponents = (id, url, callback) => {
       }
     });
 };
+
+window.onload = () => {
+  loadComponents("navbar", "./src/sections/navbar.html", () => {
+    const menuBtn = document.getElementById("menu-btn");
+    const closeBtn = document.getElementById("close-btn");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    menuBtn.addEventListener("click", () => {
+      mobileMenu.classList.remove("hidden");
+      menuBtn.classList.add("hidden");
+    });
+
+    closeBtn.addEventListener("click", () => {
+      mobileMenu.classList.add("hidden");
+      menuBtn.classList.remove("hidden");
+    });
+  });
+
+  loadComponents("footer", "./src/sections/footer.html");
+};
